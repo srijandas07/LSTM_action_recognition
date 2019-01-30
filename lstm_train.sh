33d4f10207e35e6d71ce36274168ed533130fb74
dairui01 <dairui0001@gmail.com>
2019-01-30 13:59:22 +0100

    Delete lstm_train.sh

--- lstm_train.sh
+++
@@ -1,5 +0,0 @@
-module load cuda/8.0 cudnn/5.1-cuda-8.0
-export PYTHONPATH=./model_scripts/:$PYTHONPATH
-export PYTHONPATH=./readers/:$PYTHONPATH
-mkdir -p weights_$2
-python $1 $2 --epochs $3 --mode $4
