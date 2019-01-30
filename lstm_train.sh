f4268e340096edfe917b422205b3dd89b5aedf90
dairui01 <dairui0001@gmail.com>
2019-01-30 14:00:53 +0100

    Add files via upload

---
+++ lstm_train.sh
@@ -0,0 +1,6 @@
+export PATH=/home/sdas/anaconda2/bin:$PATH
+module load cuda/8.0 cudnn/5.1-cuda-8.0
+export PYTHONPATH=./model_scripts/:$PYTHONPATH
+export PYTHONPATH=./readers/:$PYTHONPATH
+mkdir -p weights_$2
+python $1 $2 --epochs $3 
