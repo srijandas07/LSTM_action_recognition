fe354432d328d0855d6ae99bbb828e89a24270d0
dairui01 <dairui0001@gmail.com>
2019-01-30 17:47:09 +0100

    Update lstm_train.sh

--- lstm_train.sh
+++ lstm_train.sh
@@ -3,4 +3,4 @@ module load cuda/8.0 cudnn/5.1-cuda-8.0
 export PYTHONPATH=./model_scripts/:$PYTHONPATH
 export PYTHONPATH=./readers/:$PYTHONPATH
 mkdir -p weights_$2
-python $1 $2 --epochs $3 
+python ./lstm_train_skeleton.py $1 $2
