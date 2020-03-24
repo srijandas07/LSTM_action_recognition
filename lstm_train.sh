ff4a24a596ae8af4815a84394c80686f10e44e9d
Srijan Das <srijandas07@gmail.com>
2020-03-24 12:17:33 +0100

    Update lstm_train.sh

--- lstm_train.sh
+++ lstm_train.sh
@@ -1,6 +1,4 @@
-export PATH=/home/sdas/anaconda2/bin:$PATH
-module load cuda/8.0 cudnn/5.1-cuda-8.0
 export PYTHONPATH=./model_scripts/:$PYTHONPATH
 export PYTHONPATH=./readers/:$PYTHONPATH
-mkdir -p weights_$2
-python ./lstm_train_skeleton.py $1 $2
+
+python main.py
